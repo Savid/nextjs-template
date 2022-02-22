8cb3086494c72f0ae0359b6a916dfd653e91a25d
Andre Davis <github@savid.tech>
2022-02-22 23:40:42 +1000

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,17 @@
 import '@styles/globals.css';
-import type { AppProps } from 'next/app';
+import { NEXT_PUBLIC_WEB_VITALS_ENDPOINT } from '@app/constants';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  if (!NEXT_PUBLIC_WEB_VITALS_ENDPOINT) return;
+  const body = JSON.stringify(metric);
+  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(NEXT_PUBLIC_WEB_VITALS_ENDPOINT, body);
+  } else {
+    fetch(NEXT_PUBLIC_WEB_VITALS_ENDPOINT, { body, method: 'POST', keepalive: true });
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
